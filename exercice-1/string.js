5b9727bc50ed5c5e8c25edce0af583847c511e52
louisdeluche <louis87@hotmail.fr>
2018-11-06 17:15:13 +0100

    [DONE] exercice 1

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -26,11 +26,10 @@ else return '';
 
 function camelCase(str) {
     if (typeof str === 'string' || str instanceof String){
-        return str
-            .replace(/\s(.)/g, function($1) { return $1.toUpperCase(); })
-            .replace(/\s/g, '')
-            .replace(/^(.)/, function($1) { return $1.toLowerCase(); });
-    }
+        return text.replace(/^([A-Z])|[\s-_]+(\w)/g, function(match, p1, p2, offset) {
+            if (p2) return p2.toUpperCase();
+            return p1.toLowerCase();
+        }
     else return '';
 
 }
